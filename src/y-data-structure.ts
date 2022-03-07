968c26f18bb7eb7b6eef53aec9fa4cc6c06dcab2
YLF <1463020554@qq.com>
2022-03-07 21:27:24 +0800

    init

---
+++ src/y-data-structure.ts
@@ -0,0 +1,10 @@
+// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
+// import "core-js/fn/array.find"
+// ...
+export default class DummyClass {
+  private name: string
+
+  constructor() {
+    this.name = '111'
+  }
+}
